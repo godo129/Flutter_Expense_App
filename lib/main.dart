5ed795c52c426e473465e3149dfc6c1d81768482
godo129 <76652929+godo129@users.noreply.github.com>
2022-06-03 00:37:00 +0900

    card setting

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,22 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
-      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceAround, // 위치 잡기
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Text('CHART!'),
+                color: Colors.blue,
+                elevation: 5,
+              ),
+            ),
+            Card(
+              color: Colors.red,
+              child: Text('LIST OF TX'),
+            )
+          ]),
     );
   }
 }
