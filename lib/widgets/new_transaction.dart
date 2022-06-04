d6568927a60f1e16ea18a603341be158021d5d55
godo129 <76652929+godo129@users.noreply.github.com>
2022-06-05 01:45:47 +0900

    Date Picker

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTx;
@@ -11,72 +11,94 @@ class NewTransaction extends StatefulWidget {
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final titleController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime _selectedDate;
 
-  final amountController = TextEditingController();
-
-  void submitData() {
-    final enteredTitle = titleController.text;
-    final enteredAmount = double.parse(amountController.text);
+  void _submitData() {
+    final enteredTitle = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
 
     if (enteredTitle.isEmpty || enteredAmount <= 0) {
       return;
     }
 
-    widget.addTx(
-      enteredTitle, 
-      enteredAmount
-    );
+    widget.addTx(enteredTitle, enteredAmount);
 
     Navigator.of(context).pop();
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(2019),
+            lastDate: DateTime.now())
+        .then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+    print('...');
   }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-              elevation: 5,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Title'),
-                      controller: titleController,
-                      onSubmitted: (_) => submitData(),
-                      // onChanged: (value) {
-                      //   titleInput = value;
-                      // },
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: _titleController,
+              onSubmitted: (_) => _submitData(),
+              // onChanged: (value) {
+              //   titleInput = value;
+              // },
+            ),
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      _selectedDate == null
+                          ? 'No Date Chosen!'
+                          : 'Picked Date : ${DateFormat.yMd().format(_selectedDate)}',
                     ),
-                    Container(
-                      height: 70,
-                      child: Row(
-                        children: <Widget>[
-                          Text('No Date Chosen!'),
-                          FlatButton(
-                            textColor: Theme.of(context).primaryColor,
-                            onPressed: () {}, 
-                            child: Text('Choose Date', style: TextStyle(fontWeight: FontWeight.bold),)
-                          )
-                        ],
-                      ),
-                    ),
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Amount'),
-                      controller: amountController,
-                      keyboardType: TextInputType.number,
-                      onSubmitted: (_) => submitData(),
-                      // onChanged: (value) => amountInput = value ,
-                    ),
-                    FlatButton(
-                      onPressed: submitData, 
-                      child: Text('Add Transaction'),
-                      color: Theme.of(context).primaryColor,
-                      textColor: Colors.white,
-                    )
-                  ],
-                ),
+                  ),
+                  FlatButton(
+                      textColor: Theme.of(context).primaryColor,
+                      onPressed: _presentDatePicker,
+                      child: Text(
+                        'Choose Date',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ))
+                ],
               ),
-            );
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: _amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => _submitData(),
+              // onChanged: (value) => amountInput = value ,
+            ),
+            RaisedButton(
+              onPressed: _submitData,
+              child: Text('Add Transaction'),
+              color: Theme.of(context).primaryColor,
+              textColor: Colors.white,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
