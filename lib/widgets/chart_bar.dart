b48cce3720828110a6184d87127dec80b48c2f1f
godo129 <76652929+godo129@users.noreply.github.com>
2022-06-05 00:04:07 +0900

    chart styling

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
-  final String label; 
-  final double spendingAmount; 
+  final String label;
+  final double spendingAmount;
   final double spendingPctOfTotal;
 
   ChartBar(this.label, this.spendingAmount, this.spendingPctOfTotal);
 
   @override
   Widget build(BuildContext context) {
-
     return Column(
       children: <Widget>[
-        Text('\$${spendingAmount.toStringAsFixed(0)}'),
-        SizedBox(height: 4,),
+        FittedBox(
+          child: Text('\$${spendingAmount.toStringAsFixed(0)}')
+        ),
+        SizedBox(
+          height: 4,
+        ),
         Container(
           height: 60,
           width: 10,
@@ -24,18 +27,16 @@ class ChartBar extends StatelessWidget {
                   border: Border.all(color: Colors.grey, width: 1.0),
                   color: Color.fromRGBO(220, 220, 220, 1),
                   borderRadius: BorderRadius.circular(10),
-
-                  ),
                 ),
-                FractionallySizedBox(
-                  heightFactor: spendingPctOfTotal, 
-                  child: Container(
+              ),
+              FractionallySizedBox(
+                heightFactor: spendingPctOfTotal,
+                child: Container(
                     decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.circular(10),
-                      )
-                  ),
-                  )
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(10),
+                )),
+              )
             ],
           ),
         ),
@@ -45,6 +46,5 @@ class ChartBar extends StatelessWidget {
         Text(label),
       ],
     );
-    
   }
 }
