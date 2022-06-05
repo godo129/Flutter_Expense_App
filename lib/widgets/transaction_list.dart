ce764b900121b55ff9c963a14f76c2d6ee22c62d
godo129 <76652929+godo129@users.noreply.github.com>
2022-06-05 14:30:01 +0900

    finish landscape mode

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,7 +12,8 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-          ? Column(
+          ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
               children: <Widget>[
                 Text(
                   'No transactions add yet!',
@@ -22,13 +23,14 @@ class TransactionList extends StatelessWidget {
                   height: 10,
                 ),
                 Container(
-                    height: 200,
+                    height: constraints.maxHeight * 0.6,
                     child: Image.asset(
                       'assets/images/waiting.png',
                       fit: BoxFit.cover,
                     )),
               ],
-            )
+            );
+          }) 
           : ListView.builder(
               itemBuilder: (ctx, index) {
                 return Card(
